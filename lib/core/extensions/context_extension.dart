ee53b26d1873ab9a388bbae9af92aef8691b982a
mengliming <13014795306@163.com>
2024-11-08 10:02:58 +0800

    getViewModel调整

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -7,25 +7,19 @@ extension ContextExtension on BuildContext {
   ///
   /// 如果需要查找共享的 ViewModel，请将 [forceInherited] 设置为 true。
   ///
-  /// 当 [ignoreCache] 为 true 时，不查找自身的ViewModel，
+  /// 当 [ignoreContainer] 为 true 时，不查找自身的ViewModel，
   /// 当 [ignoreStack] 为 true 时，不查找堆栈中的ViewModel，
   /// 当 [ignoreInherited] 为 true 时，不查找Inherited中的，
   ///
   /// [inheritedListen] 参数决定是否在 ViewModel 发生变化时重新构建。
   T? getViewModel<T extends ViewModel>({
-    bool ignoreCache = false,
+    bool ignoreContainer = false,
     bool ignoreStack = false,
     bool ignoreInherited = false,
     bool inheritedListen = false,
   }) {
-    // 如果不强制查找共享 ViewModel
-    if (!ignoreCache) {
-      T? result;
-      // 检查当前上下文是否实现了 ConsumerStatefulElement
-      if (this is ConsumerStatefulElement) {
-        // 从缓存中获取 ViewModel
-        result = (this as ConsumerStatefulElement).getViewModel<T>();
-      }
+    if (!ignoreContainer && this is ConsumerStatefulElement) {
+      final result = (this as ConsumerStatefulElement).getViewModel<T>();
       // 如果找到缓存的 ViewModel，直接返回
       if (result != null) {
         return result;
